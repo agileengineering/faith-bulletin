966d74f973648496ee8363a3b189028b600440e9
Shane Ardell <shane.ardell@gmail.com>
2016-07-30 14:04:43 -0400

    Add status to tasks.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :user, optional: true
 
+  enum status: {
+    incomplete:  1,
+    complete:    2,
+  }
+
   validates :title,           presence: true, length: { in: 1..255 }
 
   def claim(u)
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730154915) do
+ActiveRecord::Schema.define(version: 20160730175537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string   "title",       null: false
+    t.string   "title",                   null: false
     t.text     "description"
     t.datetime "deadline"
     t.integer  "user_id"
+    t.integer  "status",      default: 1, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
