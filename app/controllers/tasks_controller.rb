7d6c77e0defec618c9767567a46ea66bd885f431
Shane Ardell <shane.ardell@gmail.com>
2016-07-30 13:14:10 -0400

    Add task edit interface.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
 
   def index
-    @tasks = Task.order('id desc')
+    @tasks = Task.order('deadline asc')
     @task  = Task.new
   end
 
@@ -11,6 +11,19 @@ class TasksController < ApplicationController
       redirect_to tasks_path, notice: 'Task successfully created.'
     else
       render action: 'index', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: 'Task successfully updated.'
+    else
+      render action: 'edit', status: :unprocessable_entity
     end
   end
 
